8086d8f314519fdff08485f1f7a7534c89c738ef
tajo48 <tajo48@proton.me>
2023-11-20 04:02:31 +0100

    a few lines of code and one episode of jojo later i now have my board

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,44 @@ struct Move {
     to: Position,
 }
 // #[warn(dead_code)]
-// struct ChessBoard {
-//     board: [[char; 8]; 8],
-// }
+struct ChessBoard {
+    board: [[char; 8]; 8],
+}
 
 fn main() {
 println!("Welcome to Rust Chess!");
-let Move { from , to } = Move { from: read_move(true), to: read_move(false)};
+let board = ChessBoard {
+    board: [['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+            ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+            ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']],
+};
 
+
+print_board(board);
+
+let Move { from, to } = Move {
+    from: read_move(true),
+    to: read_move(false),
+};
 println!("Your piece moves from {}{} to {}{}", from.x, from.y, to.x, to.y);
 }
 
-// fn print_board(board: ChessBoard) {
-//     for i in 0..8 {
-//         for j in 0..8 {
-//             print!("{}", board.board[i][j]);
-//         }
-//         println!("");
-//     }
-// }
+fn print_board(board: ChessBoard) {
+    //TODO print board nicely with letters and numbers
+    println!("abcdefgh");
+    for i in 0..8 {
+        for j in 0..8 {
+            print!("{}", board.board[i][j]);
+        }
+        println!("");
+    }
+    println!("abcdefgh");
+}
 
 // fn move_validation(Move { from, to }: Move) -> bool
 // {
@@ -60,32 +79,28 @@ fn read_move(fromorwhere: bool) -> Position {
     //convert input to lowercase
     input = input.to_lowercase();
     
-    //firt char must be a letter if not return read_move_from()
-    if !input.chars().next().unwrap().is_alphabetic() {
-        println!("Invalid input first char is not a letter");
-        return read_move(fromorwhere);
-    }
-    
-    //first char must be a letter between a and h if not return read_move_from()
-    if input.chars().next().unwrap() < 'a' || input.chars().next().unwrap() > 'h' {
-        println!("Invalid input first char is not a letter between a and h");
-        return read_move(fromorwhere);
-    }
-
+        //firt char must be a letter if not return read_move_from()
+        if !input.chars().next().unwrap().is_alphabetic() {
+            println!("Invalid input first char is not a letter");
+            return read_move(fromorwhere);
+        }
+        //first char must be a letter between a and h if not return read_move_from()
+        if input.chars().next().unwrap() < 'a' || input.chars().next().unwrap() > 'h' {
+            println!("Invalid input first char is not a letter between a and h");
+            return read_move(fromorwhere);
+        }
     let first_char = input.chars().next().unwrap();
-    
-    //second char must be a number if not return read_move_from()
-    if !input.chars().nth(1).unwrap().is_numeric() {
-        println!("Invalid input second char is not a number");
-        return read_move(fromorwhere);
-    }
-    
-    //second char must be a number between 1 and 8 if not return read_move_from()
-    if input.chars().nth(1).unwrap().to_digit(10).unwrap() < 1 || input.chars().nth(1).unwrap().to_digit(10).unwrap() > 8 {
-        println!("Invalid input second char is not a number between 1 and 8");
-        return read_move(fromorwhere);
-    }
 
+        //second char must be a number if not return read_move_from()
+        if !input.chars().nth(1).unwrap().is_numeric() {
+            println!("Invalid input second char is not a number");
+            return read_move(fromorwhere);
+        }
+        //second char must be a number between 1 and 8 if not return read_move_from()
+        if input.chars().nth(1).unwrap().to_digit(10).unwrap() < 1 || input.chars().nth(1).unwrap().to_digit(10).unwrap() > 8 {
+            println!("Invalid input second char is not a number between 1 and 8");
+            return read_move(fromorwhere);
+        }
     let second_char = input.chars().nth(1).unwrap().to_digit(10).unwrap() as u8;
     
 
